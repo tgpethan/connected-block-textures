8f1ecdf73baa04e876b9349b273d56430e90466b
IMS212 <31803019+IMS212@users.noreply.github.com>
2021-06-15 08:48:16 -0700

    Update to 1.17

--- src/main/java/io/github/nuclearfarts/cbt/mixin/ReloadableResourceManagerImplMixin.java
+++ src/main/java/io/github/nuclearfarts/cbt/mixin/ReloadableResourceManagerImplMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.resource.ReloadableResourceManager;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourceReloadMonitor;
+import net.minecraft.resource.ResourceReload;
 import net.minecraft.util.Unit;
 
 import io.github.nuclearfarts.cbt.ConnectedBlockTextures;
@@ -24,8 +24,8 @@ public abstract class ReloadableResourceManagerImplMixin implements ReloadableRe
 	@Shadow
 	public abstract void addPack(ResourcePack resourcePack);
 
-	@Inject(method = "beginMonitoredReload", at = @At(value = "RETURN", shift = At.Shift.BEFORE))
-	private void injectCBTPack(Executor prepareExecutor, Executor applyExecutor, CompletableFuture<Unit> initialStage, List<ResourcePack> packs, CallbackInfoReturnable<ResourceReloadMonitor> cir) {
+	@Inject(method = "reload", at = @At(value = "RETURN", shift = At.Shift.BEFORE))
+	private void injectCBTPack(Executor prepareExecutor, Executor applyExecutor, CompletableFuture<Unit> initialStage, List<ResourcePack> packs, CallbackInfoReturnable<ResourceReload> cir) {
 		ConnectedBlockTextures.RESOURCE_PACK_PRIORITY_MAP.clear();
 		for(int i = 0; i < packs.size(); i++) {
 			ConnectedBlockTextures.RESOURCE_PACK_PRIORITY_MAP.put(packs.get(i).getName(), i);
